d5fd61e054b317b3823ba05f1c2a795e6d5b7193
GentleMizt <olamideiyanda18@gmail.com>
2023-04-12 22:20:36 +0100

    progress

---
+++ lancer.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close-modal');
+const hideClass = document.querySelector('.hidden');
+const overlay = document.querySelector('.overlay');
