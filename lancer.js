03259fe899d4ce9617ede3fdda7ffe049509ab4f
GentleMizt <olamideiyanda18@gmail.com>
2023-04-12 22:29:48 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -1,7 +1,14 @@
 'use strict';
 
-const showModal = document.querySelectorAll('.show-modal');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close-modal');
+const showModals = document.querySelectorAll('.show-modal');
+const modalWindow = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
 const hideClass = document.querySelector('.hidden');
 const overlay = document.querySelector('.overlay');
+
+// Nodelist is a little bit like an array but at the same time it's not an array.
+
+for (let i = 0; i < showModals.length; i++) {
+    const modal  = showModals[i];
+    console.log(modal.textContent);
+}
